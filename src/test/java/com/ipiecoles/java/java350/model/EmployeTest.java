5ee1a150b9507427948d7e7d8615e0053ce0c902
georgeabedgit config --global user.name georgeabedgit config --global user.name georgeabed <georges.abed@hotmail.com>
2020-03-26 00:42:05 +0100

    Tests unitaires classiques

---
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,69 @@
+package com.ipiecoles.java.java350.model;
+
+import java.time.LocalDate;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EmployeTest {
+	
+	@Test
+	public void testNbAnneeAncienneteNow() {
+		
+		//Given
+		Employe employe = new Employe();
+		employe.setDateEmbauche(LocalDate.now());
+		
+		//When
+		Integer nbAnnees = employe.getNombreAnneeAnciennete();
+		
+		//Then
+		Assertions.assertThat (nbAnnees).isEqualTo(0);
+	}
+	
+	@Test
+	public void testNbAnneeAncienneteNowMinus2() {
+			
+			//Given
+			Employe employe = new Employe();
+			employe.setDateEmbauche(LocalDate.now().minusYears(2));
+			
+			//When
+			Integer nbAnnees = employe.getNombreAnneeAnciennete();
+			
+			//Then
+			Assertions.assertThat (nbAnnees).isEqualTo(2);
+		
+	}
+	
+	@Test
+	public void testNbAnneeAncienneteNowPlus3() {
+			
+			//Given
+			Employe employe = new Employe();
+			employe.setDateEmbauche(LocalDate.now().plusYears(3));
+			
+			//When
+			Integer nbAnnees = employe.getNombreAnneeAnciennete();
+			
+			//Then
+			Assertions.assertThat (nbAnnees).isEqualTo(0);
+		
+	}
+	
+	@Test
+	public void testNbAnneeAncienneteNowNull() {
+			
+			//Given
+			Employe employe = new Employe();
+			employe.setDateEmbauche(null);
+			
+			//When
+			Integer nbAnnees = employe.getNombreAnneeAnciennete();
+			
+			//Then
+			Assertions.assertThat (nbAnnees).isEqualTo(0);
+		
+	}
+
+}
